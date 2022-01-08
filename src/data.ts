1c5767b11051776b2e9233696afa15a435da0631
gktin <gktin@yandex.ru>
2022-01-08 13:02:15 +0700

    навыки

--- src/data.ts
+++ src/data.ts
@@ -153,7 +153,6 @@ export const ru: Resume = {
                 ['Git'],
                 ['HTML & CSS'],
                 ['TypeScript'],
-                ['TypeScript'],
                 ['JavaScript'],
                 ['Webpack'],
                 ['StyledComponents']
