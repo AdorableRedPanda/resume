8befe4e9c8f56b6d18bec541064e06078172ab4b
gotin <gotin@tecforce.ru>
2022-11-04 07:31:38 +0400

    add vite, remove webpack

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: './src',
+    server: {
+        port: '8080',
+    },
+    build: {
+        outDir: '../dist',
+        assetsDir: './',
+    }
+});
